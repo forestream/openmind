d764ed9a0f16500e455dbf04c7908a9c9856a15a
hanbin <rj2303@naver.com>
2024-03-14 17:23:31 +0900

    feat: 유저 생성 리스폰스 에러 핸들 추가

--- src/components/apis/postUser.js
+++ src/components/apis/postUser.js
@@ -6,18 +6,21 @@ const postUser = async (userName) => {
     team: "4-12"
   };
 
-  const response = await fetch(`${BASE_URL}` + `/subjects/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(USER_DATA)
-  });
+  try {
+    const response = await fetch(`${BASE_URL}` + `/subjects/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(USER_DATA)
+    });
 
-  const result = await response.json();
+    const result = await response.json();
 
-  console.log(result);
-  return result;
+    return result;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default postUser;
