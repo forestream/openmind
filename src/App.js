f161a25bad76b3bee7eaa23eb420cf64e842af5b
hanbin <rj2303@naver.com>
2024-03-14 18:58:30 +0900

    feat: 라우터 돔 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GenerateUser from "./components/GenerateUser";
 
 function App() {
-  return <GenerateUser />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GenerateUser />} />
+        <Route path="/post">
+          <Route path=":userId">
+            <Route index />
+            <Route path="answer"></Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
